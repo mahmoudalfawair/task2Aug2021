c875c2c1416dfb4dac222753108295e4944ea229
mahmoud <84728207+mahmoudalfawair@users.noreply.github.com>
2021-08-02 12:15:12 +0300

    Add files via upload

---
+++ task2Aug2021B.cpp
@@ -0,0 +1,12 @@
+/* multiply 5*/
+#include <iostream>
+using namespace std;
+int main()
+{
+int plus=1;
+for(; plus<=5; plus++)
+{for(int i =1; i <13; i++)
+cout << "\n"<< plus << " * " << i<<" ="<< plus*i ;
+cout << endl;
+}
+return 2;}
