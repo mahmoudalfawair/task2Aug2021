5bee6ea2453baf04f2a6ffd67071437aae0292d2
mahmoud <84728207+mahmoudalfawair@users.noreply.github.com>
2021-08-02 11:58:23 +0300

    Add files via upload

---
+++ task2Aug2021.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+cout << "Enter the number : " ;
+int num;
+cin >> num;
+int res;
+res=pow(num,3);
+cout << "\nthe cube of this number is : "<< res << endl;
+
+return 2;
+}
---
+++ task2Aug2021B.cpp
@@ -0,0 +1,8 @@
+/* multiply 5*/
+#include <iostream>
+using namespace std;
+int main()
+{
+for(int i =1; i <13; i++)
+cout << "\n"<< i << "* " << "5 ="<< i*5;
+return 2;}
