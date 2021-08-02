d3f1cbfba533f27479f9ae4b8684229afed4d4c5
mahmoud <84728207+mahmoudalfawair@users.noreply.github.com>
2021-08-02 12:18:46 +0300

    Update task2Aug2021.cpp

--- task2Aug2021.cpp
+++ task2Aug2021.cpp
@@ -6,9 +6,7 @@ int main()
 cout << "Enter the number : " ;
 int num;
 cin >> num;
-int res;
-res=pow(num,3);
-cout << "\nthe cube of this number is : "<< res << endl;
+cout << "\nthe cube of this number is : "<< num*num*num << endl;
 
 return 2;
 }
